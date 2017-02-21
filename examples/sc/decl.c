371abdf9b5338f77003c86195c727dcbd6ad7f79
jetou <1021644861@qq.com>
2017-02-21 20:30:06 +0800

    linux

---
+++ examples/sc/decl.c
@@ -0,0 +1,59 @@
+/*************************
+*		declaration
+ *				int declarator
+*///////////////////////////////
+#include <stdio.h>
+#include "lex.h"
+ #include "expr.h"
+ #include "error.h"
+ #include "decl.h"
+ static AstNodePtr DirectDeclarator(void);
+ static AstNodePtr PostfixDeclarator(void);
+ static AstNodePtr Declarator(void);
+/*////////////////////////////////////////////////////////////////////////
+   direct-declarator:
+ 		ID
+ 		( declarator )
+ */
+static AstNodePtr DirectDeclarator(void){
+	AstNodePtr directDecl = NULL;
+	if(curToken.kind == TK_ID){
+		directDecl = CreateAstNode(TK_ID,&curToken.value,NULL,NULL);
+		NEXT_TOKEN;
+	}else if(curToken.kind == TK_LPAREN){
+		NEXT_TOKEN;
+		directDecl = Declarator();
+		Expect(TK_RPAREN);
+	}
+	return directDecl;
+}
+/*****************************
+   postfix-declarator:
+ 		direct-declarator
+ 		postfix-declarator [num]
+ 		postfix-declarator (void)
+ 		*/
+ static AstNodePtr PostfixDeclarator(void){
+ 	AstNodePtr decl = DirectDeclarator();
+ 	while(1){
+ 		if(curToken.kind == TK_LBRACKET){
+ 			NEXT_TOKEN;
+ 			decl = CreateAstNode(TK_ARRAY,&curToken.value,NULL,decl);
+ 			if(curToken.kind == TK_NUM){
+ 				decl->value.numVal = curToken.value.numVal;
+ 				NEXT_TOKEN;
+ 			}else{
+ 				decl->value.numVal=0;
+ 			}
+ 			Expect(TK_RBRACKET);
+ 		}else if(curToken.kind == TK_LPAREN){
+ 			AstNodePtr * param;
+ 			NEXT_TOKEN;
+ 			decl = CreateAstNode(TK_FUNCTION,&curToken.value,NULL,decl);
+ 			param=&(decl->kids[0]);
+ 			while(curToken.kind == TK_INT){
+ 				*param = CreateAstNode(TK_INT, &curToken.value,NULL,NULL)
+ 			}
+ 		}
+ 	}
+ }
