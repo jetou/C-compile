ef7ee936155d6f1b761e664907ad8985b65cb602
jetou <1021644861@qq.com>
2017-02-27 22:13:58 +0800

    windows

---
+++ examples/diysc/lex.h
@@ -0,0 +1,21 @@
+#ifndef LEX_H
+#define LEX_H
+
+#define MAX_ID_LEN 15
+
+typedef union {
+	char name[MAX_ID_LEN];
+	int numVal;
+}Value;
+
+typedef enum{
+	#define TOKEN(kind,name) Kind,
+	#include "tokens.txt"
+	#undef TOKEN
+}TokenKind;
+
+typedef struct{
+	TokenKind kind;
+	Value value;
+}Token;
+
